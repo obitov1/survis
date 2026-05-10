089d9a2ee14da2665b8b79e6d92cad59e3895a85
obitov1 <1017733149@qq.com>
2026-05-10 21:29:29 +0100

    Update properties.js

--- src/properties.js
+++ src/properties.js
@@ -2,7 +2,7 @@
  * Name as used as title for the main page
  * @type {string}
  */
-var title = 'SurVis Literature Collection';
+var title = 'LLMs in Software Engineering Literature Collection';
 
 /**
  * Relative path to the data directory
@@ -30,55 +30,52 @@ var stylesDir = 'styles/';
  * @type {{field: string, title: string, minTagFrequency: number}[]}
  */
 var tagCloudOptions = [{
-    field: 'keywords',
-    title: 'Keywords',
-    minTagFrequency: 1
+  field: 'keywords',
+  title: 'Keywords',
+  minTagFrequency: 1
 }, {
-    field: 'author',
-    title: 'Authors',
-    minTagFrequency: 1
+  field: 'author',
+  title: 'Authors',
+  minTagFrequency: 1
 }, {
-    field: 'series',
-    title: 'Series',
-    minTagFrequency: 1
+  field: 'series',
+  title: 'Research Areas',
+  minTagFrequency: 1
 }];
 
 /**
- * If BibTeX entries (and tags) should be editable
+ * If BibTeX entries and tags should be editable
  * @type {boolean}
  */
 var editable = true;
 
 /**
- * Subtitle describing the paper the data is referring to (set to null to deactivate)
+ * Subtitle describing the paper the data is referring to
+ * Set to null to deactivate
  * @type {{html: string, id: string}}
  */
 var paper = {
-    html: 'Sample literature collection for <b><a href="http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/eurovis14-star.pdf" target="_blank">SurVis</a></b>',
-    id: 'Beck2016Visual'
+  html: 'A literature collection on <b>Large Language Models in Software Engineering</b>',
+  id: 'Hou2024LLMSE'
 };
 
 /**
- * Extra pages like an about page that will be listed in the footer an can be opened on demand (page name -> path)
+ * Extra pages like an about page that will be listed in the footer and can be opened on demand
  */
 var extraPages = {
-    'about': 'about.html'
+  'about': 'about.html'
 };
 
 /**
- * Custom style as path to an extra css file (leave empty if not applicable)
+ * Custom style as path to an extra css file
+ * Leave empty if not applicable
  * @type {string}
  */
 var customStyle = '';
-/* Example for simplified entries */
-//var customStyle = 'styles/custom_entries_simplified.css';
 
 /**
- * Options for showing citation information (set to null to avoid showing any citation information)
- * minCitationCount: minimum number of citations of a paper to be displayed in the visualization
+ * Options for showing citation information
+ * Set to null to avoid showing any citation information
  * @type {{minCitationCount: number}}
  */
-/* var citations = {
-    minCitationCount: 5
-}; */
 var citations = null;
