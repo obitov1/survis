7ae5c43b661ab27cc14cceee08835110ede701c2
obitov1 <1017733149@qq.com>
2026-05-10 20:34:52 +0000

    Update literature collection

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,106 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "Arvidsson2023Prompt": {
+        "author": "Arvidsson, Simon and Axell, Joel",
+        "keywords": "llm, prompt-engineering, requirements-engineering",
+        "school": "University of Gothenburg and Chalmers University of Technology",
+        "title": "Prompt Engineering Guidelines for LLMs in Requirements Engineering",
+        "type": "mastersthesis Bachelor of Science Thesis in Software Engineering and Management",
+        "year": "2023"
+    },
+    "Daun2023ChatGPT": {
+        "author": "Daun, Marian and Brings, Jennifer",
+        "booktitle": "Proceedings of the 2023 Conference on Innovation and Technology in Computer Science Education V. 1",
+        "doi": "10.1145/3587102.3588815",
+        "keywords": "chatgpt, llm, software-engineering-education, education",
+        "pages": "110--116",
+        "title": "How ChatGPT Will Change Software Engineering Education",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "Fan2023LLMSE": {
+        "author": "Fan, Angela and Gokkaya, Bihuan and Harman, Mark and Lyubarskiy, Max and Sengupta, Subhajit and Yoo, Shin and Zhang, Jie M.",
+        "booktitle": "2023 IEEE/ACM International Conference on Software Engineering: Future of Software Engineering",
+        "doi": "10.1109/ICSE-FoSE59343.2023.00008",
+        "keywords": "llm, software-engineering, survey, open-problems",
+        "pages": "31--53",
+        "title": "Large Language Models for Software Engineering: Survey and Open Problems",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "Hou2024LLMSE": {
+        "author": "Hou, Xinyi and Zhao, Yanjie and Liu, Yue and Yang, Zhou and Wang, Kailong and Li, Li and Luo, Xiapu and Lo, David and Grundy, John and Wang, Haoyu",
+        "doi": "10.1145/3695988",
+        "journal": "ACM Transactions on Software Engineering and Methodology",
+        "keywords": "llm, software-engineering, systematic-review, survey",
+        "number": "8",
+        "pages": "1--79",
+        "title": "Large Language Models for Software Engineering: A Systematic Literature Review",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "volume": "33",
+        "year": "2024"
+    },
+    "Imai2022Copilot": {
+        "author": "Imai, Saki",
+        "booktitle": "Proceedings of the 44th International Conference on Software Engineering Companion",
+        "doi": "10.1145/3510454.3522684",
+        "keywords": "github-copilot, code-generation, pair-programming, empirical-study",
+        "pages": "319--321",
+        "title": "Is GitHub Copilot a Substitute for Human Pair-programming? An Empirical Study",
+        "type": "inproceedings",
+        "year": "2022"
+    },
+    "Nejjar2025LLMScience": {
+        "author": "Nejjar, Mohamed and Zacharias, Lisa and Stiehle, Fabian and Weber, Ingo",
+        "doi": "10.1002/smr.2723",
+        "journal": "Journal of Software: Evolution and Process",
+        "keywords": "llm, code-generation, data-analysis, science",
+        "pages": "e2723",
+        "title": "LLMs for Science: Usage for Code Generation and Data Analysis",
+        "type": "article",
+        "volume": "37",
+        "year": "2025"
+    },
+    "Odong2022Requirements": {
+        "author": "Odong, Lawrence A. and Perini, Anna and Susi, Angelo",
+        "booktitle": "Research Challenges in Information Science",
+        "doi": "10.1007/978-3-031-05760-1_24",
+        "keywords": "requirements-engineering, collaborative-ai, literature-survey",
+        "pages": "409--425",
+        "publisher": "Springer",
+        "title": "Requirements Engineering for Collaborative Artificial Intelligence Systems: A Literature Survey",
+        "type": "incollection",
+        "year": "2022"
+    },
+    "Wang2024Testing": {
+        "author": "Wang, Junjie and Huang, Yuchao and Chen, Chunrong and Liu, Zhe and Wang, Song and Wang, Qing",
+        "doi": "10.1109/TSE.2024.3368208",
+        "journal": "IEEE Transactions on Software Engineering",
+        "keywords": "llm, software-testing, survey",
+        "number": "4",
+        "pages": "911--936",
+        "title": "Software Testing with Large Language Models: Survey, Landscape, and Vision",
+        "type": "article",
+        "volume": "50",
+        "year": "2024"
+    },
+    "WangChen2023CodeGeneration": {
+        "author": "Wang, Jing and Chen, Y.",
+        "booktitle": "2023 IEEE International Conference on Medical Artificial Intelligence",
+        "doi": "10.1109/MedAI59581.2023.00044",
+        "keywords": "llm, code-generation, application, evaluation, review",
+        "pages": "284--289",
+        "title": "A Review on Code Generation with LLMs: Application and Evaluation",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "Zhang2023Copilot": {
+        "author": "Zhang, Bo and Liang, Peng and Zhou, Xin and Ahmad, Aakash and Waseem, Muhammad",
+        "booktitle": "Proceedings of the 35th International Conference on Software Engineering and Knowledge Engineering",
+        "doi": "10.18293/SEKE2023-077",
+        "keywords": "github-copilot, empirical-study, practices, challenges",
+        "pages": "308--313",
+        "title": "Practices and Challenges of Using GitHub Copilot: An Empirical Study",
+        "type": "inproceedings",
+        "year": "2023"
     }
 };
